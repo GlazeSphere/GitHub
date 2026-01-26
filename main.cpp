b3b32c6a69612536ddbfea78f1929c9b3d8db73c
GlazeSphere <zjiahui2021@gmail.com>
2026-01-26 11:55:35 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "Vector3D.hpp"
+
+int main() {
+    Vector3D v1; //default constructor
+    Vector3D v2(2.244, 0.0001, 4.0); //parameterized constructor 
+    v1.display();
+    v2.display();
+    return 0;
+}
